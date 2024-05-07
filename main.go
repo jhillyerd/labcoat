c3d5163c15fded2d3353bc9bfe24b45a75d501dc
James Hillyerd <james@hillyerd.com>
2024-05-06 19:11:40 -0700

    init viewport
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"math"
 	"os"
 
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/jhillyerd/labui/internal/runner"
 )
 
 type model struct {
-	hostList hostListModel
-	status   map[string]string
-	sizes    layoutSizes
+	ready        bool // true once screen size is known.
+	hostList     hostListModel
+	contentPanel viewport.Model
+	status       map[string]string
+	sizes        layoutSizes
 }
 
 type layoutSizes struct {
@@ -68,6 +71,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.sizes = calculateSizes(msg)
 		m.hostList.SetSize(m.sizes.hostList.width, m.sizes.hostList.height)
 
+		if m.ready {
+			m.contentPanel.Width = m.sizes.contentPanel.width
+			m.contentPanel.Height = m.sizes.contentPanel.height
+		} else {
+			// First size message, init content viewport.
+			m.contentPanel = viewport.New(m.sizes.contentPanel.width, m.sizes.contentPanel.height)
+			m.ready = true
+		}
 		return m, nil
 
 	case tea.Cmd:
@@ -85,6 +96,11 @@ var contentPanelStyle = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), true
 
 // View implements tea.Model.
 func (m model) View() string {
+	// Delay rendering until screen dimensions are known.
+	if !m.ready {
+		return "\n"
+	}
+
 	hosts := hostListStyle.Render(m.hostList.View())
 
 	status := "\n"
@@ -94,7 +110,8 @@ func (m model) View() string {
 			status = text
 		}
 	}
-	content := contentPanelStyle.Render(status)
+	m.contentPanel.SetContent(status)
+	content := contentPanelStyle.Render(m.contentPanel.View())
 
 	return lipgloss.JoinHorizontal(lipgloss.Top, hosts, content)
 }
@@ -119,11 +136,12 @@ func calculateSizes(msg tea.WindowSizeMsg) layoutSizes {
 	var frameX, frameY int
 
 	frameX, frameY = hostListStyle.GetFrameSize()
-	s.hostList.width = int(math.Max(10, float64(msg.Width)*0.2)) - frameX
+	hostListWidth := int(math.Max(10, float64(msg.Width)*0.2))
+	s.hostList.width = hostListWidth - frameX
 	s.hostList.height = msg.Height - frameY
 
 	frameX, frameY = contentPanelStyle.GetFrameSize()
-	s.contentPanel.width = int(math.Max(10, float64(msg.Width)*0.2)) - frameX
+	s.contentPanel.width = msg.Width - hostListWidth - frameX
 	s.contentPanel.height = msg.Height - frameY
 
 	return s
