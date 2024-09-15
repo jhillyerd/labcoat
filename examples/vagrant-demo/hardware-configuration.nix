ef0d2dc62a2c1fc1ed2398d2dc454592009113df
James Hillyerd <james@hillyerd.com>
2024-09-15 11:10:48 -0700

    feat: add Vagrant powered demo
    Signed-off-by: James Hillyerd <james@hillyerd.com>

---
+++ examples/vagrant-demo/hardware-configuration.nix
@@ -0,0 +1,28 @@
+# Vagrant + kvm hardware config.
+{ lib, modulesPath, ... }:
+{
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "virtio_pci" "virtio_scsi" "floppy" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/f101d11d-cf4b-4705-96ee-bf21580ee386";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.ens4.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
