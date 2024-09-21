033ff2e0f45d15f62681f048f51aebb36a152991
James Hillyerd <james@hillyerd.com>
2024-09-21 15:58:44 -0700

    feat: add Vagrant powered demo (#7)
    Signed-off-by: James Hillyerd <james@hillyerd.com>

---
+++ examples/vagrant-demo/flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "labcoat vagrant-demo system flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+    in
+    {
+      nixosConfigurations =
+        let
+          # Configuration shared by all systems.
+          commonModule = {
+            boot.loader.grub.enable = true;
+            boot.loader.grub.device = "/dev/vda";
+
+            boot.initrd.checkJournalingFS = false;
+
+            services.openssh = {
+              enable = true;
+              settings.PermitRootLogin = "yes";
+            };
+
+            system.stateVersion = "24.05";
+          };
+
+          # Creates a network module based on provided config.
+          networkModule = { hostName, ipAddr }: {
+            _module.args.deployAddr = ipAddr; # See labcoat deploy-host-attr config.
+            networking = {
+              inherit hostName;
+              interfaces = {
+                ens6.ipv4.addresses = [{
+                  address = ipAddr;
+                  prefixLength = 24;
+                }];
+              };
+            };
+          };
+        in
+        {
+          db-lab = lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              commonModule
+              ./hardware-configuration.nix
+              (networkModule { hostName = "db-lab"; ipAddr = "192.168.33.10"; })
+            ];
+          };
+
+          web-lab = lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              commonModule
+              ./hardware-configuration.nix
+              (networkModule { hostName = "web-lab"; ipAddr = "192.168.33.11"; })
+            ];
+          };
+        };
+    };
+}
