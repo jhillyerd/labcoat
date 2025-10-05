0c77cc77b2f0e5ae94f2146a2ee6ca2a598cfad2
James Hillyerd <james@hillyerd.com>
2025-10-05 14:08:59 -0700

    fix: rewrite runner async buffer interfaces
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- internal/runner/buf.go
+++ internal/runner/buf.go
@@ -1,11 +1,15 @@
 package runner
 
-import "sync"
+import (
+	"io"
+	"sync"
+)
 
 type buffer struct {
 	sync.RWMutex
 	buf    []byte
 	notify func() // Called when data is written to this buffer.
+	closed bool   // No more writes accepted when true.
 }
 
 func newBuffer(notify func()) *buffer {
@@ -16,15 +20,27 @@ func newBuffer(notify func()) *buffer {
 
 // Write implements io.Writer.
 func (b *buffer) Write(p []byte) (int, error) {
+	b.Lock()
+	defer b.Unlock()
+
+	if b.closed {
+		return 0, io.ErrClosedPipe
+	}
+
 	n := len(p)
 	if n > 0 {
-		b.Lock()
-		defer b.Unlock()
 		b.buf = append(b.buf, p...)
 		b.notify()
 	}
 
 	return n, nil
+}
+
+// Close prevents further writes.
+func (b *buffer) Close() {
+	b.Lock()
+	defer b.Unlock()
+	b.closed = true
 }
 
 func (b *buffer) String() string {
