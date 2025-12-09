7e4ffd0238b569f221ffce43fd8e96f95ed01f92
James Hillyerd <james@hillyerd.com>
2025-12-08 19:22:38 -0800

    chore: update screen recording (#33)
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- examples/vagrant-demo/flake.nix
+++ examples/vagrant-demo/flake.nix
@@ -2,7 +2,7 @@
   description = "labcoat vagrant-demo system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
   outputs = { nixpkgs, ... }:
